bc450d09a9e6234d534d4f2b28d2893f753012fa
Hardik <manavp4196@gmail.com>
2018-04-19 10:05:36 -0400

    initial commit

---
+++ app/src/main/java/com/example/hardik/dialogdemo/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.hardik.dialogdemo;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.AlertDialogLayout;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    Button b2;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    button=(Button) findViewById(R.id.button);
+    b2=(Button) findViewById(R.id.button2);
+
+
+    button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this  );
+            alert.setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Toast.makeText(MainActivity.this, "Yes clicked", Toast.LENGTH_SHORT).show();
+                }
+            });
+            alert.setNegativeButton("no", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Toast.makeText(MainActivity.this, "No not Clicked", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            alert.setTitle("Here is title");
+            alert.setMessage("Here is message to show");
+
+
+            AlertDialog alertDialog = alert.create();
+            alertDialog.show();
+
+
+        }
+    });
+
+    b2.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            final Dialog dialog = new Dialog(MainActivity.this);
+
+            dialog.setContentView(R.layout.alertlayout);
+            dialog.setTitle("Title here");
+            Button button = (Button) dialog.findViewById(R.id.button3);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(MainActivity.this, "Bye Bye", Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                }
+            });
+
+            dialog.show();
+        }
+    });
+
+
+    }
+}
